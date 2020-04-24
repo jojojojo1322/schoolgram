fc107a1b165a9b1ead00f6dbf3eb61016767e328
jojojojo1322 <bobob935@gmail.com>
2020-04-24 18:10:47 +0900

    nodemailer + mailgun test success

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,33 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
 import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-mailgun-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+// nodemailer + mailgun --> env + utils + server.js
+const sendMail = (email) => {
+  const options = {
+    auth: {
+      domain: process.env.MAILGUN_DOMAIN,
+      api_key: process.env.MAILGUN_API,
+    },
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (adress, secret) => {
+  const email = {
+    from: "bobob935@naver.com",
+    to: adress,
+    subject: "🔒Login Secret for Schoolgram🔒",
+    html: `Hello! Your login secret it ${secret}.<br/>Copy paste on the app/website to log in`,
+  };
+  return sendMail(email);
 };
